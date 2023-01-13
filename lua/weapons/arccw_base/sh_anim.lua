93e22e41b3af1a1f1b6284c2c158d4ba3eedf594
Артем <bydlo.codd@bk.ru>
2023-01-13 15:49:25 +0300

    Localize some functions inside SWEP:PlayAnimation()

--- lua/weapons/arccw_base/sh_anim.lua
+++ lua/weapons/arccw_base/sh_anim.lua
@@ -54,194 +54,213 @@ end
 local inf = math.huge
 local minf = -inf
 
-function SWEP:PlayAnimation(key, mult, pred, startfrom, tt, skipholster, priority, absolute)
-    mult = mult or 1
-    pred = pred or false
-    startfrom = startfrom or 0
-    tt = tt or false
-    --skipholster = skipholster or false Unused
-    priority = priority or false
-    absolute = absolute or false
-    if not key then return end
+do
+    local PLAYER = FindMetaTable("Player")
+    local playerGetViewModel = PLAYER.GetViewModel
+    local playerAddVCDSequenceToGestureSlot = PLAYER.AddVCDSequenceToGestureSlot
 
-    local ct = CurTime()
+    local ENTITY = FindMetaTable("Entity")
+    local entityLookupSequence = ENTITY.LookupSequence
+    local entitySelectWeightedSequence = ENTITY.SelectWeightedSequence
+    local entityGetPos = ENTITY.GetPos
+    local entitySendViewModelMatchingSequence = ENTITY.SendViewModelMatchingSequence
+    local entitySequenceDuration = ENTITY.SequenceDuration
+    local entitySetPlaybackRate = ENTITY.SetPlaybackRate
+    local entityGetAttachment = ENTITY.GetAttachment
+    local entityWorldToLocalAngles = ENTITY.WorldToLocalAngles
 
-    if self:GetPriorityAnim() and not priority then return end
+    local util_SharedRandom = util.SharedRandom
 
-    local owner = self:GetOwner()
-    local ownerIsPlayer = owner:IsPlayer()
+    function SWEP:PlayAnimation(key, mult, pred, startfrom, tt, skipholster, priority, absolute)
+        mult = mult or 1
+        pred = pred or false
+        startfrom = startfrom or 0
+        tt = tt or false
+        --skipholster = skipholster or false Unused
+        priority = priority or false
+        absolute = absolute or false
+        if not key then return end
 
-    if isSingleplayer and SERVER and pred then
-        net.Start("arccw_sp_anim")
-        net.WriteString(key)
-        net.WriteFloat(mult)
-        net.WriteFloat(startfrom)
-        net.WriteBool(tt)
-        --net.WriteBool(skipholster) Unused
-        net.WriteBool(priority)
-        net.Send(owner)
-    end
+        local ct = CurTime()
 
-    local anim = self.Animations[key]
-    if not anim then return end
-    local tranim = self:GetBuff_Hook("Hook_TranslateAnimation", key)
-    if self.Animations[tranim] then
-        key = tranim
-        anim = self.Animations[tranim]
-    --[[elseif self.Animations[key] then -- Can't do due to backwards compatibility... unless you have a better idea?
-        anim = self.Animations[key]
-    else
-        return]]
-    end
+        if self:GetPriorityAnim() and not priority then return end
 
-    local isFirstTimePredicted = IsFirstTimePredicted()
+        local owner = self:GetOwner()
+        local ownerIsPlayer = owner:IsPlayer()
 
-    if CLIENT and anim.ViewPunchTable then
-        for k, v in ipairs(anim.ViewPunchTable) do
-
-            if !v.t then continue end
-
-            local st = (v.t * mult) - startfrom
-
-            if st >= 0 and isnumber(v.t) and ownerIsPlayer and (isSingleplayer or isFirstTimePredicted) then
-                self:SetTimer(st, function() self:OurViewPunch(v.p or vector_origin) end, id)
-            end
+        if isSingleplayer and SERVER and pred then
+            net.Start("arccw_sp_anim")
+            net.WriteString(key)
+            net.WriteFloat(mult)
+            net.WriteFloat(startfrom)
+            net.WriteBool(tt)
+            --net.WriteBool(skipholster) Unused
+            net.WriteBool(priority)
+            net.Send(owner)
         end
-    end
 
-    if isnumber(anim.ShellEjectAt) then
-        self:SetTimer(anim.ShellEjectAt * mult, function()
-            local num = 1
-            if self.RevolverReload then
-                num = self.Primary.ClipSize - self:Clip1()
-            end
-            for i = 1, num do
-                self:DoShellEject()
-            end
-        end)
-    end
-
-    if not owner then return end
-    if not owner.GetViewModel then return end
-    local vm = owner:GetViewModel()
-
-    if not IsValid(vm) then return end
-
-    local now = CurTime()
-
-    local seq = anim.Source
-    if anim.RareSource and util.SharedRandom("raresource", 0, 1, now) < (1 / (anim.RareSourceChance or 100)) then
-        seq = anim.RareSource
-    end
-    seq = self:GetBuff_Hook("Hook_TranslateSequence", seq)
-
-    if istable(seq) then
-        seq["BaseClass"] = nil
-        seq = seq[math.Round(util.SharedRandom("randomseq" .. now, 1, #seq))]
-    end
-
-    if isstring(seq) then
-        seq = vm:LookupSequence(seq)
-    end
-
-    local time = absolute and 1 or self:GetAnimKeyTime(key)
-    local timeMult = time * mult
-    local ttime = timeMult - startfrom
-
-    -- if startfrom > timeMult then return end
-    if ttime < 0 then
-        return
-    end
-
-    if tt then
-        self:SetNextPrimaryFire(ct + ((anim.MinProgress or time) * mult) - startfrom)
-    end
-
-    if anim.LHIK then
-        self.LHIKStartTime = ct
-        self.LHIKEndTime = ct + ttime
-
-        if anim.LHIKTimeline then
-            -- self.LHIKTimeline = {}
-            local timeline = {}
-            local animTimeline = anim.LHIKTimeline
-            for i = 1, #animTimeline do
-                timeline[i] = animTimeline[i]
-            end
-
-            self.LHIKTimeline = timeline
+        local anim = self.Animations[key]
+        if not anim then return end
+        local tranim = self:GetBuff_Hook("Hook_TranslateAnimation", key)
+        if self.Animations[tranim] then
+            key = tranim
+            anim = self.Animations[tranim]
+        --[[elseif self.Animations[key] then -- Can't do due to backwards compatibility... unless you have a better idea?
+            anim = self.Animations[key]
         else
-            local lhikInDefault = anim.LHIKIn or 0.1
-            local lhikOutDefault = anim.LHIKOut or 0.1
+            return]]
+        end
+    
+        local isFirstTimePredicted = IsFirstTimePredicted()
 
-            self.LHIKTimeline = {
-                {t = minf, lhik = 1},
-                {t = (lhikInDefault - (anim.LHIKEaseIn or lhikInDefault)) * mult, lhik = 1},
-                {t = lhikInDefault * mult, lhik = 0},
-                {t = ttime - (lhikOutDefault * mult), lhik = 0},
-                {t = ttime - ((lhikOutDefault - (anim.LHIKEaseOut or lhikOutDefault)) * mult), lhik = 1},
-                {t = inf, lhik = 1}
-            }
-
-            if anim.LHIKIn == 0 then
-                self.LHIKTimeline[1].lhik = minf
-                self.LHIKTimeline[2].lhik = minf
-            end
-
-            if anim.LHIKOut == 0 then
-                local len = #self.LHIKTimeline
-                self.LHIKTimeline[len - 1].lhik = inf
-                self.LHIKTimeline[len].lhik = inf
+        if CLIENT and anim.ViewPunchTable then
+            for k, v in ipairs(anim.ViewPunchTable) do
+    
+                if !v.t then continue end
+    
+                local st = (v.t * mult) - startfrom
+    
+                if st >= 0 and isnumber(v.t) and ownerIsPlayer and (isSingleplayer or isFirstTimePredicted) then
+                    self:SetTimer(st, function() self:OurViewPunch(v.p or vector_origin) end, id)
+                end
             end
         end
-    else
-        self.LHIKTimeline = nil
-    end
 
-    if anim.LastClip1OutTime then
-        self.LastClipOutTime = ct + ((anim.LastClip1OutTime * mult) - startfrom)
-    end
+        if isnumber(anim.ShellEjectAt) then
+            self:SetTimer(anim.ShellEjectAt * mult, function()
+                local num = 1
+                if self.RevolverReload then
+                    num = self.Primary.ClipSize - self:Clip1()
+                end
+                for i = 1, num do
+                    self:DoShellEject()
+                end
+            end)
+        end
 
-    if anim.TPAnim then
-        local aseq = owner:SelectWeightedSequence(anim.TPAnim)
-        if aseq then
-            owner:AddVCDSequenceToGestureSlot( GESTURE_SLOT_ATTACK_AND_RELOAD, aseq, anim.TPAnimStartTime or 0, true )
-            if !isSingleplayer and SERVER then
-                net.Start("arccw_networktpanim")
-                    net.WriteEntity(owner)
-                    net.WriteUInt(aseq, 16)
-                    net.WriteFloat(anim.TPAnimStartTime or 0)
-                net.SendPVS(owner:GetPos())
+        if not owner then return end
+        if not owner.GetViewModel then return end
+        local vm = playerGetViewModel(owner)
+    
+        if not IsValid(vm) then return end
+
+        local now = CurTime()
+
+        local seq = anim.Source
+        if anim.RareSource and util_SharedRandom("raresource", 0, 1, now) < (1 / (anim.RareSourceChance or 100)) then
+            seq = anim.RareSource
+        end
+        seq = self:GetBuff_Hook("Hook_TranslateSequence", seq)
+
+        if istable(seq) then
+            seq["BaseClass"] = nil
+            seq = seq[math.Round(util_SharedRandom("randomseq" .. now, 1, #seq))]
+        end
+
+        if isstring(seq) then
+            seq = entityLookupSequence(vm, seq)
+        end
+
+        local time = absolute and 1 or self:GetAnimKeyTime(key)
+        local timeMult = time * mult
+        local ttime = timeMult - startfrom
+
+        -- if startfrom > timeMult then return end
+        if ttime < 0 then
+            return
+        end
+
+        if tt then
+            self:SetNextPrimaryFire(ct + ((anim.MinProgress or time) * mult) - startfrom)
+        end
+
+        if anim.LHIK then
+            self.LHIKStartTime = ct
+            self.LHIKEndTime = ct + ttime
+
+            if anim.LHIKTimeline then
+                -- self.LHIKTimeline = {}
+                local timeline = {}
+                local animTimeline = anim.LHIKTimeline
+                for i = 1, #animTimeline do
+                    timeline[i] = animTimeline[i]
+                end
+
+                self.LHIKTimeline = timeline
+            else
+                local lhikInDefault = anim.LHIKIn or 0.1
+                local lhikOutDefault = anim.LHIKOut or 0.1
+
+                self.LHIKTimeline = {
+                    {t = minf, lhik = 1},
+                    {t = (lhikInDefault - (anim.LHIKEaseIn or lhikInDefault)) * mult, lhik = 1},
+                    {t = lhikInDefault * mult, lhik = 0},
+                    {t = ttime - (lhikOutDefault * mult), lhik = 0},
+                    {t = ttime - ((lhikOutDefault - (anim.LHIKEaseOut or lhikOutDefault)) * mult), lhik = 1},
+                    {t = inf, lhik = 1}
+                }
+
+                if anim.LHIKIn == 0 then
+                    self.LHIKTimeline[1].lhik = minf
+                    self.LHIKTimeline[2].lhik = minf
+                end
+
+                if anim.LHIKOut == 0 then
+                    local len = #self.LHIKTimeline
+                    self.LHIKTimeline[len - 1].lhik = inf
+                    self.LHIKTimeline[len].lhik = inf
+                end
+            end
+        else
+            self.LHIKTimeline = nil
+        end
+
+        if anim.LastClip1OutTime then
+            self.LastClipOutTime = ct + ((anim.LastClip1OutTime * mult) - startfrom)
+        end
+
+        if anim.TPAnim then
+            local aseq = entitySelectWeightedSequence(owner, anim.TPAnim)
+            if aseq then
+                playerAddVCDSequenceToGestureSlot(owner, GESTURE_SLOT_ATTACK_AND_RELOAD, aseq, anim.TPAnimStartTime or 0, true)
+                if !isSingleplayer and SERVER then
+                    net.Start("arccw_networktpanim")
+                        net.WriteEntity(owner)
+                        net.WriteUInt(aseq, 16)
+                        net.WriteFloat(anim.TPAnimStartTime or 0)
+                    net.SendPVS(entityGetPos(owner))
+                end
             end
         end
-    end
 
-    if not (isSingleplayer and CLIENT) then
-        -- self.EventTable = {}
-        if isSingleplayer or (!isSingleplayer and isFirstTimePredicted) then
-            self:PlaySoundTable(anim.SoundTable or {}, 1 / mult, startfrom, key)
+        if not (isSingleplayer and CLIENT) then
+            -- self.EventTable = {}
+            if isSingleplayer or (!isSingleplayer and isFirstTimePredicted) then
+                self:PlaySoundTable(anim.SoundTable or {}, 1 / mult, startfrom, key)
+            end
         end
+
+        if seq then
+            entitySendViewModelMatchingSequence(vm, seq)
+            local dur = entitySequenceDuration(vm)
+            entitySetPlaybackRate(vm, math.Clamp(dur / (ttime + startfrom), -4, 12))
+            self.LastAnimStartTime = ct
+            self.LastAnimFinishTime = ct + dur
+            self.LastAnimKey = key
+        end
+
+        local att = self:GetBuff_Override("Override_CamAttachment") or self.CamAttachment -- why is this here if we just... do cool stuff elsewhere?
+        if att then
+            local attachmentData = entityGetAttachment(vm, att)
+            if attachmentData then
+                self.Cam_Offset_Ang = entityWorldToLocalAngles(vm, attachmentData.Ang)
+            end
+        end
+
+        self:SetNextIdle(now + ttime)
+
+        return true
     end
-
-    if seq then
-        vm:SendViewModelMatchingSequence(seq)
-        local dur = vm:SequenceDuration()
-        vm:SetPlaybackRate(math.Clamp(dur / (ttime + startfrom), -4, 12))
-        self.LastAnimStartTime = ct
-        self.LastAnimFinishTime = ct + dur
-        self.LastAnimKey = key
-    end
-
-    local att = self:GetBuff_Override("Override_CamAttachment") or self.CamAttachment -- why is this here if we just... do cool stuff elsewhere?
-    if att and vm:GetAttachment(att) then
-        local ang = vm:GetAttachment(att).Ang
-        ang = vm:WorldToLocalAngles(ang)
-        self.Cam_Offset_Ang = Angle(ang)
-    end
-
-    self:SetNextIdle(now + ttime)
-
-    return true
 end
 
 function SWEP:PlayIdleAnimation(pred)
